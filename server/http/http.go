82decde75627f0cc5486e1250a4f0c94cc5eb658
whxmail <191908577@qq.com>
2016-11-01 12:59:39 +0800

    rename http

--- server/http/http.go
+++ server/http/http.go
@@ -11,7 +11,7 @@ import (
 func main() {
     http.HandleFunc("/", sayhelloName)       //设置访问的路由
     http.HandleFunc("/login", login)         //设置访问的路由
-    err := http.ListenAndServe(":9090", nil) //设置监听的端口
+    err := http.ListenAndServe(":8080", nil) //设置监听的端口
     if err != nil {
         log.Fatal("ListenAndServe: ", err)
     }
---
+++ server/imap/client2.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+    "log"
+
+    "github.com/emersion/go-imap/client"
+    "github.com/emersion/go-imap"
+)
+
+func main() {
+    log.Println("Connecting to server...")
+
+    // Connect to server
+    c, err := client.DialTLS("imap.qq.com:993", nil)
+    if err != nil {
+        log.Fatal(err)
+    }
+    log.Println("Connected")
+
+    // Don't forget to logout
+    defer c.Logout()
+
+    // Login
+    if err := c.Login("lbzhello@qq.com", "bgryiivldhlybhcj"); err != nil {
+        log.Fatal(err)
+    }
+    log.Println("Logged in")
+
+    // List mailboxes
+    mailboxes := make(chan *imap.MailboxInfo)
+    done := make(chan error, 1)
+    go func () {
+        done <- c.List("", "*", mailboxes)
+    }()
+
+    log.Println("Mailboxes:")
+    for m := range mailboxes {
+        log.Println("* " + m.Name)
+    }
+
+    if err := <-done; err != nil {
+        log.Fatal(err)
+    }
+
+    // Select INBOX
+    mbox, err := c.Select("INBOX", false)
+    if err != nil {
+        log.Fatal(err)
+    }
+    log.Println("Flags for INBOX:", mbox.Flags)
+
+
+    // Get the last 4 messages
+    seqset, _ := imap.NewSeqSet("")
+    seqset.AddRange(mbox.Messages - 3, mbox.Messages)
+
+    messages := make(chan *imap.Message)
+    done = make(chan error, 1)
+    go func() {
+        done <- c.Fetch(seqset, []string{imap.EnvelopeMsgAttr}, messages)
+    }()
+
+    for msg := range messages {
+        log.Println(msg.Envelope.Subject)
+    }
+
+    if err := <-done; err != nil {
+        log.Fatal(err)
+    }
+
+    log.Println("Done!")
+}
